caf94e43c0c4ec6b38707a62b841ed7decbfdec9
s33721 <s33721@pjwstk.edu.pl>
2025-06-05 11:15:26 +0200

    created constructor and overriden methods

--- src/GasContainer.java
+++ src/GasContainer.java
@@ -1,2 +1,24 @@
-public class GasContainer {
+public class GasContainer extends Container implements IHazardNotifier{
+    public GasContainer(double mass, double height, double tareWeight, double weightOfCargo, double depth, double maximumPayload){
+        super(mass, height, tareWeight, weightOfCargo, depth, maximumPayload);
+    }
+    @Override
+    public void setSerialNumber() {
+
+    }
+
+    @Override
+    public String getSerialNumber() {
+        return "";
+    }
+
+    @Override
+    public void loadCargo() {
+
+    }
+
+    @Override
+    public void notifyHazard() {
+
+    }
 }
