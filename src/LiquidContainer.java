dbdfd1d94bbdb1db22be9923797adfb4c09524d4
s33721 <s33721@pjwstk.edu.pl>
2025-06-06 23:46:43 +0200

    fixed liquid container

--- src/LiquidContainer.java
+++ src/LiquidContainer.java
@@ -1,22 +1,12 @@
 public class LiquidContainer extends Container implements IHazardNotifier {
-    public LiquidContainer(double mass, double height, double tareWeight, double weightOfCargo, double depth, double maximumPayload) {
-        super(mass, height, tareWeight, weightOfCargo, depth, maximumPayload);
+    public LiquidContainer(double mass, double height, double tareWeight, double weightOfCargo, double depth, double maximumPayload, char containerType) {
+        super(mass, height, tareWeight, weightOfCargo, depth, maximumPayload, containerType = 'L');
     }
 
     @Override
     public void notifyHazard() {
         System.out.println("This cargo is hazardous");
         System.out.println("Container number: " + this.serialNumber);
-    }
-
-    @Override
-    public void setSerialNumber() {
-        this.serialNumber = "KON-L-" + Math.random();
-    }
-
-    @Override
-    public String getSerialNumber() {
-        return this.serialNumber;
     }
 
     @Override
