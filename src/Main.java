1f1d60e4a3adbdc6b165c126780eb913a1ea2c4a
s33721 <s33721@pjwstk.edu.pl>
2025-06-07 13:41:34 +0200

    finished the task (fuck this)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        Product p = new Product("banana", 16.5);
-        p.setProductName("strawberry");
-        p.getProductName();
     }
 }
