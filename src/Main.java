1cb58fa04c5ade3ff8cf0a8152c2e9ab07f08105
s33721 <s33721@pjwstk.edu.pl>
2025-06-15 00:03:35 +0200

    dima

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) throws OverfillException {
         ArrayList<Container> ship = new ArrayList<>();
         Container petrolContainer = new LiquidContainer(200, 180, 2200, 2000, true);
-        petrolContainer.loadCargo(35);
+        petrolContainer.loadCargo(222222222);
+        ship.add(petrolContainer);
+
     }
 }
